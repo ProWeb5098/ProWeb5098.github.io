97ec947f13c657903a08e9bd6f9279d4648f96ad
ProWeb5098 <ProWeb5098@gmail.com>
2021-05-25 19:36:48 -0500

    cambios auth

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyCNEdQr1BWbwTHiDf1UIEF0MQI1bWrw8vQ",
+  authDomain: "proweb5098-68787.firebaseapp.com",
+  projectId: "proweb5098-68787",
+  storageBucket: "proweb5098-68787.appspot.com",
+  messagingSenderId: "494719463932",
+  appId: "1:494719463932:web:bd3ad3614ddb7374294279",
+  measurementId: "G-WJQVWZRHXN",
+});
